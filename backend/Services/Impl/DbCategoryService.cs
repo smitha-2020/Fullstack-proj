8f3abdce8a25c6db76842b3785fb4c0af5058968
smitha kamath <smitha.bhandarkar2014@gmail.com>
2023-03-11 00:11:16 +0200

    CRUD and Category servcies

---
+++ backend/Services/Impl/DbCategoryService.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+using backend.DB;
+using backend.DTOs;
+using backend.Models;
+using backend.Services.Impl;
+using backend.Services;
+
+namespace project.services.Impl;
+
+public class DbCategoryService : DbCrudService<Category, DTOCategory>, ICategoryService
+{
+    private readonly AppDBContext _dbContext;
+    public DbCategoryService(AppDBContext dbContext) : base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<ICollection<Category>> GetByNameOrder()
+    {
+        return await Task.Run(() => _dbContext.Categorys.OrderBy(x => x.Id).ToList());
+    }
+}
